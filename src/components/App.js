767d97763d105227623818cd95ffacf864a9509c
Serban Bondor <serbanbondor@yahoo.com>
2020-04-11 18:55:59 +0300

    added some component and context in react

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import TaskList from './TaskList';
+import TaskListContextProvider from '../context/TaskListContext';
+import '../App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <TaskListContextProvider>
+      <div>
+        <TaskList />
+      </div>
+    </TaskListContextProvider>
+  );
 };
 
 export default App;
